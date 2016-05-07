d6d5bddfcad7125ea50a04c695a506f427871103
Der-Eddy <dereddyteddy@gmail.com>
2016-05-07 22:36:17 +0200

    Added check if secretword is set

--- tbm_dashboard.user.js
+++ tbm_dashboard.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://github.com/Der-Eddy/epvp-tbm-dashboard/raw/master/tbm_dashboard.user.js
 // @author      Der-Eddy
 // @license     GNU General Public v3 <http://www.gnu.org/licenses/>
-// @version     1.1b1
+// @version     1.1b2
 // @grant       none
 // ==/UserScript==
 
@@ -31,7 +31,7 @@
   var hash = window.location.hash;
   var tbmapi = $('a[href*="type=all"]').attr('href');
   //console.log(hash);
-  console.log(tbmapi);
+  //console.log(tbmapi);
 
   //$("td.cbbg:eq(2)").prepend('<div class="cb2h"><img src="https://www.elitepvpers.com/images/tbm/trades.gif" style="float:left" alt="" height="16" width="16"> &nbsp; <a href="https://www.elitepvpers.com/theblackmarket/api/paymentwall/#dashboard" rel="nofollow">TBM Dashboard</a></div>');
   $('a[href*="https://www.elitepvpers.com/theblackmarket/namechange/"]').parent().parent().prepend('<div class="cb2h"><img src="https://www.elitepvpers.com/images/tbm/trades.gif" style="float:left" alt="" height="16" width="16"> &nbsp; <a href="https://www.elitepvpers.com/theblackmarket/api/paymentwall/#dashboard" rel="nofollow">TBM Dashboard</a></div>');
@@ -39,56 +39,61 @@
   if(hash == "#dashboard"){
     document.title = "TBM Dashboard";
     $("td.contentwhite").empty();
-    $("td.contentwhite").append('Loading ... <img src="https://www.elitepvpers.com/forum/images/misc/progress.gif">');
+
+    if(typeof tbmapi == "undefined"){
+      $("td.contentwhite").append('You must set a <a href="https://www.elitepvpers.com/theblackmarket/api/secretword/">TBM API secretword</a> (to anything you want)');
+    } else {
+      $("td.contentwhite").append('Loading ... <img src="https://www.elitepvpers.com/forum/images/misc/progress.gif">');
 
 
-    $.ajax({url:tbmapi,type:"GET",dataType:"json", success:function(jdata)
-            {
-              $("td.contentwhite").empty();
-              $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Total TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
-              $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Last Month TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
-              $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Graphical Analysis</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
-
-              var userid = tbmapi.match(/u=(\d+)/);
-              //console.log(userid);
-              userid = userid[1];
-              var r = 0;
-              var s = 0;
-              var rmonth = 0;
-              var smonth = 0;
-              var hr=0;
-              var hrobj;
-              var hs = 0;
-              var hsobj;
-              var d = new Date();
-              d.setMonth(d.getMonth() - 1);
-              d.setHours(0,0,0);
-              var date = d/1000|0;
-
-              for(var i=0;i<jdata.length;i++)
+      $.ajax({url:tbmapi,type:"GET",dataType:"json", success:function(jdata)
               {
-                if(parseInt(jdata[i].eg_from) == userid)
+                $("td.contentwhite").empty();
+                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Total TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Last Month TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Graphical Analysis</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+
+                var userid = tbmapi.match(/u=(\d+)/);
+                //console.log(userid);
+                userid = userid[1];
+                var r = 0;
+                var s = 0;
+                var rmonth = 0;
+                var smonth = 0;
+                var hr=0;
+                var hrobj;
+                var hs = 0;
+                var hsobj;
+                var d = new Date();
+                d.setMonth(d.getMonth() - 1);
+                d.setHours(0,0,0);
+                var date = d/1000|0;
+
+                for(var i=0;i<jdata.length;i++)
                 {
-                  s+=parseInt(jdata[i].amount);
-                  if(parseInt(jdata[i].amount) > hs){ hs = parseInt(jdata[i].amount); hsobj = jdata[i] }
-                  if(parseInt(jdata[i].dateline) > date){ smonth+=parseInt(jdata[i].amount); }
-                }else{
-                  r+=parseInt(jdata[i].amount);
-                  if(parseInt(jdata[i].amount) > hr){ hr = parseInt(jdata[i].amount); hrobj = jdata[i] }
-                  if(parseInt(jdata[i].dateline) > date){ rmonth+=parseInt(jdata[i].amount); }
+                  if(parseInt(jdata[i].eg_from) == userid)
+                  {
+                    s+=parseInt(jdata[i].amount);
+                    if(parseInt(jdata[i].amount) > hs){ hs = parseInt(jdata[i].amount); hsobj = jdata[i] }
+                    if(parseInt(jdata[i].dateline) > date){ smonth+=parseInt(jdata[i].amount); }
+                  }else{
+                    r+=parseInt(jdata[i].amount);
+                    if(parseInt(jdata[i].amount) > hr){ hr = parseInt(jdata[i].amount); hrobj = jdata[i] }
+                    if(parseInt(jdata[i].dateline) > date){ rmonth+=parseInt(jdata[i].amount); }
+                  }
                 }
+
+                $("div.cwalt:eq(0)").append('Received: <span class="green">' + r + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
+                $("div.cwalt:eq(0)").append('Sent: <span class="red">' + s + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
+                $("div.cwalt:eq(0)").append('<br>Highest amount received: <span class="green">' + hrobj.amount + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> from ' + hsobj.eg_tousername + '<br>');
+                $("div.cwalt:eq(0)").append('Highest amount sent: <span class="red">' + hsobj.amount + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> to ' + hsobj.eg_tousername + '<br>');
+
+                $("div.cwalt:eq(1)").append('Received last month: <span class="green">' + rmonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
+                $("div.cwalt:eq(1)").append('Sent last month: <span class="red">' + smonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
+
+                $("div.cwalt:eq(2)").append('Coming soon?');
               }
-
-              $("div.cwalt:eq(0)").append('Received: <span class="green">' + r + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
-              $("div.cwalt:eq(0)").append('Sent: <span class="red">' + s + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
-              $("div.cwalt:eq(0)").append('<br>Highest amount received: <span class="green">' + hrobj.amount + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> from ' + hsobj.eg_tousername + '<br>');
-              $("div.cwalt:eq(0)").append('Highest amount sent: <span class="red">' + hsobj.amount + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> to ' + hsobj.eg_tousername + '<br>');
-
-              $("div.cwalt:eq(1)").append('Received last month: <span class="green">' + rmonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
-              $("div.cwalt:eq(1)").append('Sent last month: <span class="red">' + smonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
-
-              $("div.cwalt:eq(2)").append('Coming soon?');
-            }
-    });
+      });
+    }
   }
 })(jQuery);
