fda49dea48d55acb158d8c7a18d555e353172021
Der-Eddy <dereddyteddy@gmail.com>
2016-05-08 22:53:54 +0200

    Added about section and some preparations for some cool charts

--- tbm_dashboard.user.js
+++ tbm_dashboard.user.js
@@ -6,8 +6,9 @@
 // @downloadURL https://github.com/Der-Eddy/epvp-tbm-dashboard/raw/master/tbm_dashboard.user.js
 // @author      Der-Eddy
 // @license     GNU General Public v3 <http://www.gnu.org/licenses/>
-// @version     1.1b2
+// @version     1.2b1
 // @grant       none
+// @require     https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.2/Chart.bundle.min.js
 // ==/UserScript==
 
 /*
@@ -32,14 +33,14 @@
   var tbmapi = $('a[href*="type=all"]').attr('href');
   //console.log(hash);
   //console.log(tbmapi);
-
+  
   //$("td.cbbg:eq(2)").prepend('<div class="cb2h"><img src="https://www.elitepvpers.com/images/tbm/trades.gif" style="float:left" alt="" height="16" width="16"> &nbsp; <a href="https://www.elitepvpers.com/theblackmarket/api/paymentwall/#dashboard" rel="nofollow">TBM Dashboard</a></div>');
   $('a[href*="https://www.elitepvpers.com/theblackmarket/namechange/"]').parent().parent().prepend('<div class="cb2h"><img src="https://www.elitepvpers.com/images/tbm/trades.gif" style="float:left" alt="" height="16" width="16"> &nbsp; <a href="https://www.elitepvpers.com/theblackmarket/api/paymentwall/#dashboard" rel="nofollow">TBM Dashboard</a></div>');
-
+  
   if(hash == "#dashboard"){
     document.title = "TBM Dashboard";
     $("td.contentwhite").empty();
-
+    
     if(typeof tbmapi == "undefined"){
       $("td.contentwhite").append('You must set a <a href="https://www.elitepvpers.com/theblackmarket/api/secretword/">TBM API secretword</a> (to anything you want)');
     } else {
@@ -51,12 +52,14 @@
                 $("td.contentwhite").empty();
                 $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Total TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
                 $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Last Month TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
-                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Graphical Analysis</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Graphical Analysis (DEMO)</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>About</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
 
+                
                 var userid = tbmapi.match(/u=(\d+)/);
                 //console.log(userid);
                 userid = userid[1];
-                var r = 0;
+                var r = 0; 
                 var s = 0;
                 var rmonth = 0;
                 var smonth = 0;
@@ -81,7 +84,7 @@
                     if(parseInt(jdata[i].amount) > hr){ hr = parseInt(jdata[i].amount); hrobj = jdata[i] }
                     if(parseInt(jdata[i].dateline) > date){ rmonth+=parseInt(jdata[i].amount); }
                   }
-                }
+                } 
 
                 $("div.cwalt:eq(0)").append('Received: <span class="green">' + r + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
                 $("div.cwalt:eq(0)").append('Sent: <span class="red">' + s + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
@@ -91,7 +94,38 @@
                 $("div.cwalt:eq(1)").append('Received last month: <span class="green">' + rmonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
                 $("div.cwalt:eq(1)").append('Sent last month: <span class="red">' + smonth + '</span> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"><br>');
 
-                $("div.cwalt:eq(2)").append('Coming soon?');
+                //$("div.cwalt:eq(2)").append('<canvas id="tbmchart" height="200"></canvas>');
+                
+                $("div.cwalt:eq(3)").append('Created by <a href="https://www.elitepvpers.com/forum/members/984054-der-eddy.html"><span style="color: green;">Der-Eddy</span></a> and released under <a href="http://www.gnu.org/licenses/">GNU General Public License v3</a> on <a href="https://github.com/Der-Eddy/epvp-tbm-dashboard">GitHub</a><br>');
+                $("div.cwalt:eq(3)").append('<a href="https://www.elitepvpers.com/forum/coding-releases/4059303-elite-gold-dashboard-deine-elite-gold-statistik.html">Elitepvpers Thread about this Project</a><br>');
+                $("div.cwalt:eq(3)").append('Donate with <a href="https://www.elitepvpers.com/theblackmarket/sendeg/984054">elite*gold</a> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> or via Bitcoin <i>33vL2Cv4bwDPDUgahcurng5ey35ditqBZc</i><br>');
+                $("div.cwalt:eq(3)").append('<br>Donation List:<ul><li>Be the first!</li></ul>');
+                
+                var ctx = document.getElementById("tbmchart");
+                var tbmChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                        datasets: [{
+                            label: 'elite*gold earned',
+                            borderColor: "rgba(0,205,0,1)",
+                            backgroundColor: "rgba(0,205,0,0.3)",
+                            lineTension: 0,
+                            fill: true,
+                            data: [12, 19, 3, 5, 2, 3]
+                        },{
+                            label: 'elite*gold spent',
+                            borderColor: "rgba(255,0,0,1)",
+                            backgroundColor: "rgba(255,0,0,0.3)",
+                            lineTension: 0,
+                            fill: true,
+                            data: [2, 9, 1, 0, 4, 10]
+                        }]
+                    },
+                    options: {
+                        responsive: true
+                    }
+                });
               }
       });
     }
