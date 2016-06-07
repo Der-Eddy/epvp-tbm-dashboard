42c2af1f165e7295a717bae0eaae433c40050da5
Der-Eddy <dereddyteddy@gmail.com>
2016-06-08 00:30:25 +0200

    Added elite*gold converter, added .ENDLOS to the donation list

--- tbm_dashboard.user.js
+++ tbm_dashboard.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://github.com/Der-Eddy/epvp-tbm-dashboard/raw/master/tbm_dashboard.user.js
 // @author      Der-Eddy
 // @license     GNU General Public License v3 <http://www.gnu.org/licenses/>
-// @version     1.2b7
+// @version     1.2b8
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.2/Chart.bundle.min.js
 // ==/UserScript==
@@ -30,6 +30,12 @@
     Chart.js is licensed under MIT: https://github.com/chartjs/Chart.js
 */
 console.log(GM_info['scriptWillUpdate']);
+
+Array.prototype.getDate = function() {
+	return this.map(function (obj) {
+		return new Date(obj * 1000); //WTF Javascript??
+	});
+};
 
 (function($){
   var hash = window.location.hash;
@@ -56,7 +62,8 @@ console.log(GM_info['scriptWillUpdate']);
                 $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Total TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
                 $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Last 30 days TBM Dashboard</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
                 $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>Graphical Analysis (DEMO)</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
-                $("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>About & Support</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+				$("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>elite*gold converter</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
+				$("td.contentwhite").append('<div><div class="cw1h"></div><div class="cwhead"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td><h2>About & Support</h2></td></tr></tbody></table></div><div class="cwalt"></div>');
 
 
                 var userid = tbmapi.match(/u=(\d+)/);
@@ -84,7 +91,6 @@ console.log(GM_info['scriptWillUpdate']);
 
                 var dmonth = new Date();
                 dmonth.setMonth(dmonth.getMonth() - 1);
-                console.log(dmonth.getMonth());
                 dmonth.setHours(0,0,0);
                 var lastMonth = dmonth/1000|0;
 
@@ -113,20 +119,41 @@ console.log(GM_info['scriptWillUpdate']);
                 $("div.cwalt:eq(1)").append('Transactions last month: ' + amonth + '<br>');
 
                 //$("div.cwalt:eq(2)").append('<canvas id="tbmchart" height="200"></canvas>');
+				$("div.cwalt:eq(2)").append('Demo deativated, sorry');
 
-                $("div.cwalt:eq(3)").append('<img src="https://www.elitepvpers.com/images/tbm/trades.gif"> Your version: <i>' + GM_info['script']['version'] + '</i> | Current version: <i>' + 'n/a' + '</i> <a href="https://github.com/Der-Eddy/epvp-tbm-dashboard/raw/master/tbm_dashboard.user.js">(Instant Update)</a><br>');
-                $("div.cwalt:eq(3)").append('<img src="https://www.elitepvpers.com/images/tbm/profile.gif"> Created by <a href="//www.elitepvpers.com/forum/members/984054-der-eddy.html"><span style="color: green;">Der-Eddy</span></a> and released under <a href="http://www.gnu.org/licenses/">GNU General Public License v3</a> on <a href="https://github.com/Der-Eddy/epvp-tbm-dashboard">GitHub</a><br>');
-                $("div.cwalt:eq(3)").append('<img src="https://www.elitepvpers.com/images/tbm/middleman.gif"> <a href="//www.elitepvpers.com/forum/coding-releases/4059303-elite-gold-dashboard-deine-elite-gold-statistik.html">Elitepvpers Thread about this Project</a><br>');
-                $("div.cwalt:eq(3)").append('<img src="https://www.elitepvpers.com/images/tbm/namechange.gif"> <a href="https://discord.gg/0ZbfSaE2dpVjIIBu">Discord Support Channel</a> (no registration needed) or send me a <a href="//www.elitepvpers.com/forum/private.php?do=newpm&u=984054">private message</a><br>');
-                $("div.cwalt:eq(3)").append('<img src="https://www.elitepvpers.com/images/tbm/premium.gif"> Donate with <a href="//www.elitepvpers.com/theblackmarket/sendeg/984054">elite*gold</a> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> or via Bitcoin <a href="https://blockchain.info/de/address/33vL2Cv4bwDPDUgahcurng5ey35ditqBZc"><i>33vL2Cv4bwDPDUgahcurng5ey35ditqBZc</i></a><br>');
-                $("div.cwalt:eq(3)").append('<br>Donation list:<ul><li><a href="//www.elitepvpers.com/forum/members/4232675-afrozilla.html"><span style="color: green;">Afrozilla</span></a> (93 elite*gold)</li><li><a href="//www.elitepvpers.com/forum/members/2074798-shura.html"><span style="color:#0099ff;">Shura</span></a> (1$)</li><li><a href="//www.elitepvpers.com/forum/members/5589432-dr-heisenberg.html"><span style="color:#0099ff;">Dr Heisenberg</span></a> (15 elite*gold)</li><li><a href="//www.elitepvpers.com/forum/members/5474128-anonymous-.html">Anonymous ™</a> (25 elite*gold)</li></ul>');
+				$("div.cwalt:eq(3)").append('<div style="text-align: center;">Insert current exchange rate<br><br>10€ = <input id="ex_rate" size="5" type="text"> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"></div><br>');
+				$("div.cwalt:eq(3)").append('<div style="text-align: center;">Converter<br><input id="euro" size="5" type="text" disabled> € <span style="font-size: 2em; margin: 10px;">&#8596</span> <input id="eg" size="5" type="text" disabled> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"></div><br>');
 
+                $("div.cwalt:eq(4)").append('<img src="https://www.elitepvpers.com/images/tbm/trades.gif"> Your version: <i>' + GM_info['script']['version'] + '</i> | Current version: <i>' + 'n/a' + '</i> <a href="https://github.com/Der-Eddy/epvp-tbm-dashboard/raw/master/tbm_dashboard.user.js">(Instant Update)</a><br>');
+                $("div.cwalt:eq(4)").append('<img src="https://www.elitepvpers.com/images/tbm/profile.gif"> Created by <a href="//www.elitepvpers.com/forum/members/984054-der-eddy.html"><span style="color: green;">Der-Eddy</span></a> and released under <a href="http://www.gnu.org/licenses/">GNU General Public License v3</a> on <a href="https://github.com/Der-Eddy/epvp-tbm-dashboard">GitHub</a><br>');
+                $("div.cwalt:eq(4)").append('<img src="https://www.elitepvpers.com/images/tbm/middleman.gif"> <a href="//www.elitepvpers.com/forum/coding-releases/4059303-elite-gold-dashboard-deine-elite-gold-statistik.html">Elitepvpers Thread about this Project</a><br>');
+                $("div.cwalt:eq(4)").append('<img src="https://www.elitepvpers.com/images/tbm/namechange.gif"> <a href="https://discord.gg/0ZbfSaE2dpVjIIBu">Discord Support Channel</a> (no registration needed) or send me a <a href="//www.elitepvpers.com/forum/private.php?do=newpm&u=984054">private message</a><br>');
+                $("div.cwalt:eq(4)").append('<img src="https://www.elitepvpers.com/images/tbm/premium.gif"> Donate with <a href="//www.elitepvpers.com/theblackmarket/sendeg/984054">elite*gold</a> <img src="https://www.elitepvpers.com/images/tbm/gold.gif"> or via Bitcoin <a href="https://blockchain.info/de/address/33vL2Cv4bwDPDUgahcurng5ey35ditqBZc"><i>33vL2Cv4bwDPDUgahcurng5ey35ditqBZc</i></a><br>');
+                $("div.cwalt:eq(4)").append('<br>Donation list:<ul><li><a href="//www.elitepvpers.com/forum/members/4232675-afrozilla.html"><span style="color: green;">Afrozilla</span></a> (93 elite*gold)</li><li><a href="//www.elitepvpers.com/forum/members/2074798-shura.html"><span style="color:#0099ff;">Shura</span></a> (1$)</li><li><a href="//www.elitepvpers.com/forum/members/5589432-dr-heisenberg.html"><span style="color:#0099ff;">Dr Heisenberg</span></a> (15 elite*gold)</li><li><a href="//www.elitepvpers.com/forum/members/5474128-anonymous-.html">Anonymous™</a> (25 elite*gold)</li><li><a href="//www.elitepvpers.com/forum/members/4000999--endlos.html">.ENDLOS</a> (100 elite*gold)</li></ul>');
+
+				//e*gold converter
+				$('#ex_rate').bind('input', function() {
+					$('#euro').removeAttr('disabled');
+				    $('#euro').val('');
+				    $('#eg').removeAttr('disabled');
+				    $('#eg').val('');
+				});
+
+				$('#euro').bind('input', function() {
+				    $('#eg').val(Math.round((parseInt($('#ex_rate').val()) / 10) * parseInt($(this).val())));
+				});
+
+				$('#eg').bind('input', function() {
+				    $('#euro').val(Math.round(((10 / parseInt($('#ex_rate').val())) * parseInt($(this).val())) * 100) / 100);
+				});
+
+				//canvas
                 var ctx = document.getElementById("tbmchart");
                 var tbmChart = new Chart(ctx, {
                     type: 'line',
                     data: {
                         //labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                        labels: dateobj,
+                        labels: dateobj.getDate(),
                         datasets: [{
                             label: 'elite*gold received',
                             borderColor: "rgba(0,205,0,1)",
@@ -152,6 +179,28 @@ console.log(GM_info['scriptWillUpdate']);
                     },
                     options: {
                         responsive: true
+                    },
+                    scales: {
+                        yAxes: [],
+                        xAxes: [{
+                            type: 'time',
+                            ticks: {
+                                fontSize: 10,
+                                maxRotation: 0,
+                                maxTicksLimit: 8
+                            },
+                            time: {
+                                unit: 'week',
+								parse: true,
+                                round: true,
+                                format: true,
+                                displayFormats: {
+                                    'week': '[W]WW - YYYY' // Week 46, or maybe "[W]WW - YYYY" ?
+                                },
+                                min: new Date(dateobj[0] * 1000),
+                                max: new Date(dateobj[-1] * 1000)
+                            }
+                        }]
                     }
                 });
               }
